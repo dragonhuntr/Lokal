e4d4c63a598aa4fca3fe44bf0a6683c8fd328a0e
Limit <31814492+dragonhuntr@users.noreply.github.com>
2025-11-13 11:14:12 -0500

    fix: pwa implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,18 +14,11 @@ export const metadata: Metadata = {
     { rel: "icon", url: "/favicon.ico" },
     { rel: "apple-touch-icon", url: "/logo.png" },
   ],
-  manifest: "/manifest.json",
   themeColor: "#000000",
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
     title: "Lokal",
-  },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
   },
   other: {
     "apple-mobile-web-app-capable": "yes",
@@ -45,6 +38,10 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
+	<head>
+	  <link rel="manifest" href="/manifest.json" />
+	  <link rel="icon" href="/favicon.ico" />
+	</head>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
         <Toaster position="top-right" richColors closeButton />
